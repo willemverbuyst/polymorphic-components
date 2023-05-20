a135933abdac42f9459b72a99454b38666d12bf2
Willem Verbuyst <willemverbuyst@gmail.com>
2023-05-20 12:19:22 +0200

    Add generic types

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="container">
       <h1>Polymorphic Components</h1>
-      <Border as="h2">test</Border>
+      <Border as="h2">title</Border>
+      <Border as="a" href="https://www.google.com">
+        link
+      </Border>
+      <Border>span</Border>
     </div>
   );
 }
--- src/components/Border.tsx
+++ src/components/Border.tsx
@@ -1,10 +1,18 @@
-interface Props {
-  as?: any;
+type BorderProps<C extends React.ElementType> = {
+  as?: C;
   children: React.ReactNode;
-}
+} & React.ComponentPropsWithoutRef<C>;
 
-export function Border({ as, children }: Props) {
+export function Border<C extends React.ElementType = "span">({
+  as,
+  children,
+  ...restProps
+}: BorderProps<C>) {
   const Component = as || "span";
 
-  return <Component className="border">{children}</Component>;
+  return (
+    <Component {...restProps} className="border">
+      {children}
+    </Component>
+  );
 }
