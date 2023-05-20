7f757eb8cab767b422f0627390f6e150807b54a4
Willem Verbuyst <willemverbuyst@gmail.com>
2023-05-20 13:58:20 +0200

    Refactor types

--- src/components/Border.tsx
+++ src/components/Border.tsx
@@ -1,14 +1,20 @@
 type Rainbow = "red" | "orange" | "green" | "blue" | "indigo" | "violet";
 
-type BorderProps<C extends React.ElementType> = {
+type AsProp<C extends React.ElementType> = {
   as?: C;
-  color?: Rainbow | "black";
 };
 
-type Props<C extends React.ElementType> = React.PropsWithChildren<
-  BorderProps<C>
-> &
-  Omit<React.ComponentPropsWithoutRef<C>, keyof BorderProps<C>>;
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> | P);
+
+type PolymorphicComponentProps<
+  C extends React.ElementType,
+  Props = object
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+type BorderProps = {
+  color?: Rainbow | "black";
+};
 
 export function Border<C extends React.ElementType = "span">({
   as,
@@ -16,14 +22,16 @@ export function Border<C extends React.ElementType = "span">({
   style,
   children,
   ...restProps
-}: Props<C>) {
+}: PolymorphicComponentProps<C, BorderProps>) {
   const Component = as || "span";
 
   const internalStyle = color ? { style: { ...style, color } } : {};
 
   return (
-    <Component {...restProps} className="border" {...internalStyle}>
-      {children}
-    </Component>
+    <div className="border">
+      <Component {...restProps} {...internalStyle}>
+        {children}
+      </Component>
+    </div>
   );
 }
